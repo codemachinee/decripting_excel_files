cf7579efad4aa4fd7f144f3b41f3bbc835ffacf1
alex levakov <codemachinee@MBP-alex.N4>
2024-02-04 02:12:12 +0300

    добавлена функция разблокировки отдельных листов

--- main.py
+++ main.py
@@ -12,18 +12,28 @@ except FileNotFoundError:
 
 
 def modify_files_in_zip(zip_file_path):
+    files_list = []
     os.makedirs('temp_dir', exist_ok=True)
     # Открываем архив для чтения и записи
     with zipfile.ZipFile(zip_file_path, 'a') as zip_ref:
         # Получаем список всех файлов в архиве
         zip_ref.extractall('temp_dir')
-        with open('temp_dir/xl/workbook.xml', 'r') as file_name:
-            content = file_name.read()
-            modified_content = modify_content(content)
-            # print(modified_content)
-        with open('temp_dir/xl/workbook.xml', 'w') as file_name:
-            file_name.write(modified_content)
-            file_name.close()
+        for i in os.listdir('temp_dir/xl/worksheets'):
+            files_list.append(f'temp_dir/xl/worksheets/{i}')
+        files_list.append('temp_dir/xl/workbook.xml')
+        for file in files_list:
+            # with open('temp_dir/xl/workbook.xml', 'r') as file_name:
+            with open(file, 'r') as file_name:
+                content = file_name.read()
+                if file == 'temp_dir/xl/workbook.xml':
+                    modified_content = modify_content(content, '<workbookProtection', 1)
+                else:
+                    modified_content = modify_content(content, '<sheetProtection', 0)
+                # print(modified_content)
+            # with open('temp_dir/xl/workbook.xml', 'w') as file_name:
+            with open(file, 'w') as file_name:
+                file_name.write(modified_content)
+                file_name.close()
     with zipfile.ZipFile(zip_file_path, 'w') as zip_ref:
         for root, dirs, files in os.walk('temp_dir'):
             for file in files:
@@ -33,17 +43,19 @@ def modify_files_in_zip(zip_file_path):
     os.rename(zip_file_path, f'{zip_file_path.replace(".zip", "")}')
 
 
-def modify_content(content):
+def modify_content(content, param_1, last_file):
     if 'Protection' in content:
-        first_index = content.find('<workbookProtection')
+        first_index = content.find(param_1)
         last_index = content.find('>', first_index)
         new_content = content[:first_index] + content[last_index + 1:]
         return new_content
-    else:
+    elif last_file == 1:
         print('шифрование не обнаружено')
         shutil.rmtree('temp_dir')
         os.rename(zip_file_path, f'{zip_file_path.replace(".zip", "")}')
         breakpoint()
+    else:
+        pass
 
 
 # Пример использования функци
